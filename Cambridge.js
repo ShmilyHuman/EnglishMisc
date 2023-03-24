2cbe8dddd95eee34fddbed4f6ea40e4e89c4c55d
ShmilyHuman <120351530+ShmilyHuman@users.noreply.github.com>
2023-03-25 01:28:40 +0700

    Update and rename 3Cambridge.js to Cambridge.js

--- Cambridge.js
+++ Cambridge.js
@@ -126,7 +126,7 @@ class Cambridge {
                 span.tran {margin:0; padding:0;}
                 span.eng_tran {margin-right:3px; padding:0;}
 		span.cloze {font-weight: 700;}
-                ul.sents {font-size:0.8em; list-style:square inside; margin:3px 0;padding:5px;background:rgba(13,71,161,0.1); border-radius:5px;}
+                ul.sents {font-size:0.9em; list-style:square inside; margin:3px 0;padding:5px;background:rgba(13,71,161,0.1); border-radius:5px;}
                 li.sent  {margin:0; padding:0;}
                 span.eng_sent {margin-right:5px;}
             </style>`;
