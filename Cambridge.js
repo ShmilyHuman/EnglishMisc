78a0f721f09125b1044849961d1aae314f3d05c3
ShmilyHuman <120351530+ShmilyHuman@users.noreply.github.com>
2023-03-24 18:08:11 +0700

    Update Cambridge.js

--- Cambridge.js
+++ Cambridge.js
@@ -123,7 +123,7 @@ class Cambridge {
             <style>
                 div.phrasehead{margin: 2px 0;font-weight: bold;}
                 span.star {color: #FFBB00;}
-                span.pos  {font-size:0.9em; margin-right:5px; padding:2px 4px; color:white; background-color:#0d47a1; border-radius:3px;}
+                span.pos  {font-size: 0.9em; padding: 0px 4px 3px 4px; color: white; border-radius: 4px; background-color: #0d47a1;}
                 span.tran {margin:0; padding:0;}
                 span.eng_tran {margin-right:3px; padding:0;}
                 ul.sents {font-size:0.9em; list-style:square inside; margin:3px 0;padding:5px;background:rgba(13,71,161,0.1); border-radius:5px;}
